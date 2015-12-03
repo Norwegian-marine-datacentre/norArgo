9334617472f10849cf0f0fa3849deb592c0c6d91
TerryHannant <thannant_github@metafu.net>
2015-12-03 14:56:52 +0100

    Initial commit

---
+++ src/main/java/no/imr/geoexplorer/arctic/roos/pojo/FloatLayer.java
@@ -0,0 +1,39 @@
+package no.imr.geoexplorer.arctic.roos.pojo;
+
+/**
+ *
+ * @author Terry Hannant <a5119>
+ */
+public class FloatLayer {
+    
+    String name;
+    String style;
+    String type;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStyle() {
+        return style;
+    }
+
+    public void setStyle(String style) {
+        this.style = style;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+    
+    
+
+}
