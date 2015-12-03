f922955dbdbfafd1940724d5a1a1b0b0add08261
TerryHannant <thannant_github@metafu.net>
2015-12-03 15:00:23 +0100

    Add queries for chart data

--- src/main/java/no/imr/geoexplorer/dao/NorArgoDao.java
+++ src/main/java/no/imr/geoexplorer/dao/NorArgoDao.java
@@ -1,13 +1,28 @@
 package no.imr.geoexplorer.dao;
 
 import java.util.List;
+import java.util.Map;
 
 import no.imr.geoexplorer.norargo.pojo.LastPositions;
 import no.imr.geoexplorer.norargo.pojo.Measurement;
 import no.imr.geoexplorer.norargo.pojo.Platform;
 
 public interface NorArgoDao {
+
     public List<Measurement> findAllMeasurement();
+
     public List<Platform> findAllPlatform();
+
     public List<LastPositions> findSisteKjentePosisjon();
+
+    public List getFloatMeasurementTypes(String platformID, String date);
+
+    public List getFloatMeasurements(String platformID, String date);
+
+    public List getFloatMeasurements(String platformID, String date, String type);
+
+    public Object getPlatformInfo(String platformID);
+
+    public Map getPlatformProfile(String platformID, String date);
+
 }
